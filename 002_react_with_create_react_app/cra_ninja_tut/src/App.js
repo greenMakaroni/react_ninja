0f0e09dc67ca29030a5fea3054f860bfb3bf31a2
greenMakaroni <mrtn.dawid@gmail.com>
2022-11-11 20:52:07 +0000

    Intro to state & useState

--- 002_react_with_create_react_app/cra_ninja_tut/src/App.js
+++ 002_react_with_create_react_app/cra_ninja_tut/src/App.js
@@ -1,26 +1,56 @@
+import { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // component state
+  const [showEvents, setShowEvents] = useState(true)
+  const [name, setName] = useState("Dawid")
+  const [events, setEvents] = useState([
+    {title: "Mario's birthday bash", id: 1},
+    {title: "Bowser's live stream", id: 2},
+    {title: "Peach's peach", id: 3}
+  ])
+
+  //callback function
+  const handleClick = () => {
+    setName('Elon')
+    console.log(name)
+  }
+
+  const handleDelete = (id) => {
+    setEvents((prev) => {
+      return prev.filter((event) => {
+        return id !== event.id
+      })
+    })
+    console.log(id)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-    
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+      !showEvents && <div>
+        <button onClick={() => setShowEvents(true)}> Show </button>
+      </div>}
+      {
+      showEvents && <div>
+        <button onClick={() => setShowEvents(false)}> Hide </button>
+      </div>}
+      <h1> My name is { name }</h1>
+      <button onClick={handleClick} > Change name </button>
+      {
+        // we're looping over events array
+        // for each event we're returning jsx template
+        // h3 with a title of the event nested inside a div with a key property set to event's id
+        showEvents && events.map((event, index) => 
+          (<div key={event.id}> 
+            <h3> {index} - {event.title} </h3>
+            <button onClick={ () => handleDelete(event.id)}> Delete event </button>
+          </div>)
+        )
+      }
     </div>
   );
 }
